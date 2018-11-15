74fccf157985ccdb575baef5fa868a54e2103f53
Filype Ferreira <filypeferreira.resource@natura.net>
2018-11-15 21:24:12 -0200

    cap 18 - validando dados do cartao

--- js/cartao.js
+++ js/cartao.js
@@ -1,45 +1,57 @@
 (function() {
     "use strict";
-    
+
     var $ = document.querySelectorAll.bind(document);
-    
-    const btnRemove = $(".opcoesDoCartao-remove");
+
+    // const btnRemove = $(".opcoesDoCartao-remove");
     // const btnsCores = $('.opcoesDoCartao-radioTipo');
-    
+
     //CONCEITO THROTTLE - PARA ESTUDAR
-    
+
     //BUBBLNG
     const cartoes = $(".cartao");
     cartoes.forEach(function(cartao) {
-        cartao.addEventListener("click", function(e) {
-            if (e.target.classList.contains("opcoesDoCartao-radioTipo")) {
-                this.style.backgroundColor = e.target.value;
-            }
-            
-            if (e.target.classList.contains("opcoesDoCartao-remove")){
-                this.classList.add("cartao--some");
-                
-                this.addEventListener('transitionend', function(){
-                    this.remove();
-                })
+
+        addEventoRemoverCartao(cartao);
+
+        cartao.addEventListener("focusin", function() {
+            cartao.classList.add("cartao--focado");
+        });
+
+        cartao.addEventListener("focusout", function() {
+            cartao.classList.remove("cartao--focado");
+        });
+
+        cartao.addEventListener("change", function(event) {
+            const elementoSelecionado = event.target;
+            const isRadioTipo = elementoSelecionado.classList.contains(
+                "opcoesDoCartao-radioTipo"
+            );
+
+            if (isRadioTipo) {
+                this.style.backgroundColor = elementoSelecionado.value;
             }
         });
-        
-        cartao.addEventListener("focusin", function(){
-            cartao.classList.add('cartao--focado');
-        })
-        
-        cartao.addEventListener('focusout', function(){
-            cartao.classList.remove('cartao--focado');
-        })
 
-        cartao.addEventListener('keydown', function(e){
-            if(e.key === "Entrer" || e.key === "Tab"){
+        cartao.addEventListener("keydown", function(e) {
+            if (e.key === "Enter" || e.key === " ") {
                 e.target.click();
             }
-        })
+        });
     });
-    
+
+    function addEventoRemoverCartao(cartao) {
+        cartao.addEventListener("click", function(e) {
+            if (e.target.classList.contains("opcoesDoCartao-remove")) {
+                this.classList.add("cartao--some");
+
+                this.addEventListener("transitionend", function() {
+                    this.remove();
+                });
+            }
+        });
+    }
+
     //SEM BUBBLNG
     // for(let i = 0; i < btnsCores.length; i++){
     //     btnsCores[i].addEventListener('click', function(){
@@ -47,18 +59,18 @@
     //         cartao.style.backgroundColor = this.value
     //     })
     // }
-    
+
     // console.log(btnsCores);
-    
+
     //REMOVENDO CARTAO SEM BUBBLNG
     // btnRemove.forEach(btn => {
     //     btn.addEventListener("click", removeCartao);
     // });
-    
+
     // function removeCartao() {
     //     const cartao = this.parentNode.parentNode;
     //     cartao.classList.add("cartao--some");
-        
+
     //     cartao.addEventListener("transitionend", function() {
     //         cartao.remove();
     //     });
---
+++ js/form.js
@@ -0,0 +1,25 @@
+(function(){
+    const form = document.querySelector('.formNovoCartao');
+
+    form.classList.remove('no-js');
+
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+        
+        const textarea = document.querySelector('.formNovoCartao-conteudo');
+
+        if(!textarea.value.trim().length){
+            const msg = document.createElement('div');
+            msg.classList.add('formNovoCartao-msg');
+            msg.textContent = 'Por favor preencher o campo corretamente';
+            
+            const btnSubmit = form.children[form.childElementCount.length-1];
+            
+            form.addEventListener("animationend", function(e){
+                e.target.remove();
+            })
+            
+            form.insertBefore(msg, btnSubmit);            
+        }
+    })
+})();
