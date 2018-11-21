6b52dc4c880d2cdd13ad221a485055ea4d7bd628
Filype Ferreira <filypeferreira.resource@natura.net>
2018-11-20 22:22:05 -0200

    cap 25

--- src/js/ajuda.js
+++ src/js/ajuda.js
@@ -1,19 +1,51 @@
 (function() {
-    const ajuda = document.querySelector("#btnAjuda");
+    const btnAjuda = document.querySelector("#btnAjuda");
 
-    ajuda.classList.remove("no-js");
+    btnAjuda.classList.remove("no-js");
 
-    ajuda.addEventListener("click", function() {
-        const ajudas = [
-            {conteudo: "Bem vindo ao Ceep", cor: '#F05450'},
-            {conteudo: "Clique no btn Linhas para mudar o layout", cor:"#92C4EC"}
-        ];
+    btnAjuda.addEventListener("click", function() {
+
+        //VIA AXIO
+        axios({
+            method: 'get',
+            url: 'https://ceep.herokuapp.com/cartoes/instrucoes',
+            responseType : 'json'
+        })
+            .then(function(response){
+                const ajudas = response.data.instrucoes;
+
+                ajudas.forEach(ajuda => {
+                    cartao.adiciona(ajuda);
+                });
+            })
+
+            
+        //VIA XML
+        // const xhr = new XMLHttpRequest();
+        // xhr.open('GET', 'https://ceep.herokuapp.com/cartoes/instrucoes');
+        // xhr.responseType = 'json';
+        // xhr.send();
+    
+        // xhr.addEventListener('load', function(){
+            
+        //     const objeto = xhr.response;
+        //     const ajudas = objeto.instrucoes;
+            
+        //     ajudas.forEach(ajuda => {
+        //         cartao.adiciona(ajuda);
+        //     });
+        // })
+
+        // const ajudas = [
+        //     {conteudo: "Bem vindo ao Ceep", cor: '#F05450'},
+        //     {conteudo: "Clique no btn Linhas para mudar o layout", cor:"#92C4EC"}
+        // ];
 
         // let msg = "";
 
-        for (let i = 0; i < ajudas.length; i++) {
-            cartao.adiciona(ajudas[i]);
-        }
+        // for (let i = 0; i < ajudas.length; i++) {
+        //     cartao.adiciona(ajudas[i]);
+        // }
 
         // const msgAlerta = document.createElement("div");
         // msgAlerta.classList.add("formNovoCartao-msg");
